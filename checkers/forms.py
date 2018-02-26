a73ca1fab2e8dab8c16dc5433cb426abd821f1ac
ivann <ivann@0xf.re>
2018-02-26 16:39:12 +0100

    prepend an underscore to internal functions name

--- checkers/forms.py
+++ checkers/forms.py
@@ -6,20 +6,6 @@ from scrapy.utils.project import get_project_settings
 from clint.textui import indent, colored, puts
 
 
-def crawler_result(item, **_):
-    """Called for each crawler result"""
-    form_found(dict(item))
-
-
-def form_found(form):
-    """Print each form found"""
-    puts(colored.green("Found form at ") + form['url'])
-    puts(colored.green("With the following field:"))
-    with indent(4):
-        for input_field in form['inputs']:
-            puts(input_field['name'])
-
-
 def check(urls, verbose):
     """Crawl a list of url"""
     settings = get_project_settings()
@@ -31,7 +17,21 @@ def check(urls, verbose):
     process = CrawlerProcess(settings)
     crawler = process.create_crawler('form')
 
-    crawler.signals.connect(crawler_result, scrapy.signals.item_scraped)
+    crawler.signals.connect(_crawler_result, scrapy.signals.item_scraped)
 
     process.crawl(crawler, urls=urls)
     process.start()
+
+
+def _crawler_result(item, **_):
+    """Called for each crawler result"""
+    _form_found(dict(item))
+
+
+def _form_found(form):
+    """Print each form found"""
+    puts(colored.green("Found form at ") + form['url'])
+    puts(colored.green("With the following field:"))
+    with indent(4):
+        for input_field in form['inputs']:
+            puts(input_field['name'])
