5aec4d4422ffd272ac5eec8d3574baf991774090
ivann <ivann@0xf.re>
2018-02-28 14:16:04 +0100

    add an helper to pretty print inputs list using tabulate
    https://bitbucket.org/astanin/python-tabulate

--- checkers/forms.py
+++ checkers/forms.py
@@ -4,6 +4,7 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from clint.textui import indent, colored, puts
+from helpers import pprint_inputs
 
 
 def check(urls, auth=None, crawl=True, robotstxt=True, verbosity=0):
@@ -41,5 +42,4 @@ def _form_found(form):
     puts(colored.green("Found form at ") + form['url'])
     puts(colored.green("With the following field:"))
     with indent(4):
-        for input_field in form['inputs']:
-            puts(input_field['name'])
+        pprint_inputs(form['inputs'])
--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 """gdprchecker helper functions"""
 from urllib.parse import urlparse
+from tabulate import tabulate
 
 
 def get_domain(url):
@@ -11,3 +12,23 @@ def default_scheme(url):
     """Add a default url scheme if missing"""
     parsed_url = urlparse(url)
     return url if parsed_url.scheme else 'http://' + url
+
+
+def dict_list_to_list_dict(dict_list):
+    """Transform a list of dictionaries to a dictionary of list"""
+    list_dict = {}
+    key_list = set(key for d in dict_list for key in d)
+    for dictionary in dict_list:
+        for key in key_list:
+            val = dictionary.get(key)
+            if key in list_dict:
+                list_dict[key].append(val)
+            else:
+                list_dict[key] = [val]
+
+    return list_dict
+
+
+def pprint_inputs(inputs):
+    """Pretty print a list of input items"""
+    print(tabulate(dict_list_to_list_dict(inputs), headers='keys'))
