2703bcc09e731f4b3333fc3379604e39bb5162ef
ivann <ivann@0xf.re>
2018-02-28 16:14:32 +0100

    print result at the end, not a item comes, this allow to check for dupes

--- checkers/forms.py
+++ checkers/forms.py
@@ -25,21 +25,48 @@ def check(urls, auth=None, crawl=True, robotstxt=True, verbosity=0):
 
     process = CrawlerProcess(settings)
     crawler = process.create_crawler('form')
+    result = Result()
 
-    crawler.signals.connect(_crawler_result, scrapy.signals.item_scraped)
+    crawler.signals.connect(result.add_item, scrapy.signals.item_scraped)
 
     process.crawl(crawler, urls=urls, crawl=crawl, auth=auth)
     process.start()
 
-
-def _crawler_result(item, **_):
-    """Called for each crawler result"""
-    _form_found(dict(item))
+    result.print(verbosity)
 
 
-def _form_found(form):
-    """Print each form found"""
-    puts(colored.green("Found form at ") + form['url'])
-    puts(colored.green("With the following field:"))
-    with indent(4):
-        pprint_inputs(form['inputs'])
+def _cmp_forms(form_a, form_b):
+    return form_a['inputs'] == form_b['inputs']
+
+
+class Result:
+    forms = []
+
+    def add_item(self, item, **_):
+        """Called for each crawler result"""
+        self._form_found(dict(item))
+
+    def _form_found(self, form):
+        duplicate = next(filter(lambda f: _cmp_forms(f, form), self.forms), None)
+        if duplicate:
+            duplicate['url'] += form['url']
+        else:
+            self.forms.append(form)
+
+    def print(self, verbosity):
+        """Print each form found"""
+        for form in self.forms:
+            url_count = len(form['url'])
+            url = ''
+            if url_count is 1:
+                url = form['url'][0]
+            elif url_count > 1:
+                url = '{count} different urls, this is probably a search or login form'.format(
+                    count=url_count)
+                if verbosity > 0:
+                    url += "\n" + str(form['url'])
+
+            puts(colored.green("Found form at ") + url)
+            puts(colored.green("With the following fields:"))
+            with indent(4):
+                pprint_inputs(form['inputs'])
